b5f07148c2b8d628530847ab958091c88fbe2457
ericjimenezbarril <167447132+ericjimenezbarril@users.noreply.github.com>
2024-12-15 09:48:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.querySelector('.btn').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('privacy-modal');
+    const acceptBtn = document.getElementById('accept-btn');
+
+    // Mostrar el modal al cargar la página
+    modal.style.display = 'flex';
+
+    // Ocultar el modal cuando se hace clic en "Aceptar"
+    acceptBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+});
